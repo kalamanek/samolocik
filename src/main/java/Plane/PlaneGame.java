ba8614e3115ada59ec9647e8506270ae4de4f275
StopsoN97 <44123580+StopsoN97@users.noreply.github.com>
2018-11-19 17:44:11 +0100

    Add files via upload

--- src/main/java/Plane/PlaneGame.java
+++ src/main/java/Plane/PlaneGame.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Plane;
 
 import java.awt.Color;
@@ -23,23 +19,9 @@ import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
-/**
- *
- * @author Student
- */
+
 public class PlaneGame extends JComponent implements ActionListener, MouseMotionListener, KeyListener {
-
-    public class CustomPair {
-
-        public int x;
-        public int y;
-
-        CustomPair() {
-            this.x = 0;
-            this.y = 0;
-        }
-
-    }
+    
     int[] tablica = new int[10];
     CustomPair[] shots = new CustomPair[1000];
 
